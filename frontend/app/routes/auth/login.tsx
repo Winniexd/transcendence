a8696ee8a1c7caa14fb8d7eccdfe6193fb9badf4
Matias Dreesen <matias.dreesen@gmail.com>
2025-08-13 22:47:28 +0200

    Added register and login page

--- frontend/app/routes/auth/login.tsx
+++ frontend/app/routes/auth/login.tsx
@@ -1,29 +1,53 @@
+import { useState } from "react"
 
-const login = () => {
+const Login = () => {
+	const [form, setForm] = useState({
+		first_name: "",
+		last_name: "",
+		username: "",
+		password: ""
+	});
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setForm({ ...form, [event.target.name]: event.target.value})
+	}
+
+	const submitForm = async (event: React.FormEvent) => {
+		event.preventDefault();
+		const res = await fetch("http://localhost:3000/login", {
+			method: "POST",
+			headers: { "Content-Type": "application/json"},
+			body: JSON.stringify(form),
+			credentials: "include"
+		})
+		const data = await res.json();
+		console.log(data)
+	}
+
 	return (
 	<div className="flex flex-col items-center align-center max-w-fit">
 		<h1 className="text-3xl font-medium">Login</h1>
-		<div className="mt-5 flex flex-col align-center gap-4">
+		<form className="m-4 flex flex-col align-center gap-4" onSubmit={submitForm}>
 			<p className="bg-gray-200 rounded-md p-2 border-2 border-solid">
-				<input placeholder="Username" className="outline-none"/>
+				<input name="username" placeholder="Username" className="outline-none" onChange={handleChange}/>
 			</p>
 			<p className="bg-gray-200 rounded-md p-2 border-2 border-solid">
-				<input type="password" placeholder="Password" className="outline-none"/>
+				<input name="password" type="password" placeholder="Password" className="outline-none" onChange={handleChange}/>
 			</p>
-			<button type="submit" className="bg-green-400 rounded-md p-2 hover:bg-green-300 hover:cursor-pointer text-center">Log In</button>
+			<button type="submit" className="bg-green-400 rounded-md p-2 border-2 border-green-500 hover:bg-green-300 hover:cursor-pointer text-center">Log In</button>
+		</form>
 			<div className="flex justify-evenly items-center gap-2">
 				<hr className="w-20 border-gray-300"></hr>
 				<p>or</p>
 				<hr className="w-20 border-gray-300"></hr>
 			</div>
-			<div className="flex items-center justify-center">
+			<div className="flex items-center justify-center mt-4">
 				<a href="http://localhost:3000/api/auth/discord/login">
 					<img src='app/assets/discord.jpeg' className="h-10 rounded-full hover:opacity-80 transition-opacity"></img>
 				</a>
 			</div>
-		</div>
 	</div>
 	)
 }
 
-export default login
+export default Login
--- frontend/app/routes/auth/register.tsx
+++ frontend/app/routes/auth/register.tsx
@@ -1,8 +1,52 @@
+import { useState } from "react"
 
-const register = () => {
+
+const Register = () => {
+	const [form, setForm] = useState({
+		first_name: "",
+		last_name: "",
+		username: "",
+		password: ""
+	});
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setForm({ ...form, [event.target.name]: event.target.value})
+	}
+
+	const submitForm = async (event: React.FormEvent) => {
+		event.preventDefault();
+		const res = await fetch("http://localhost:3000/register", {
+			method: "POST",
+			headers: { "Content-Type": "application/json"},
+			body: JSON.stringify(form),
+			credentials: "include"
+		})
+		const data = await res.json();
+		console.log(data)
+	}
+
 	return (
-		<h1>testing register</h1>
+	<div className="flex flex-col items-center align-center max-w-fit">
+		<h1 className="text-3xl font-medium">Register</h1>
+		<div className="m-4 flex flex-col align-center gap-4">
+			<form method="post" encType="" className="flex flex-col gap-3" onSubmit={submitForm}>
+				<p className="bg-gray-200 rounded-md p-2 border-2 border-solid">
+					<input name="first_name" placeholder="First Name" className="outline-none" autoCapitalize="on" value={form.first_name} onChange={handleChange}/>
+				</p>
+				<p className="bg-gray-200 rounded-md p-2 border-2 border-solid">
+					<input name="last_name" placeholder="Last Name" className="outline-none" autoCapitalize="on" value={form.last_name} onChange={handleChange}/>
+				</p>
+				<p className="bg-gray-200 rounded-md p-2 border-2 border-solid">
+					<input name="username" placeholder="Username*" className="outline-none" value={form.username} onChange={handleChange} required/>
+				</p>
+				<p className="bg-gray-200 rounded-md p-2 border-2 border-solid">
+					<input name="password" type="password" placeholder="Password*" className="outline-none" value={form.password} onChange={handleChange} required/>
+				</p>
+				<button type="submit" className="bg-green-400 rounded-md p-2 hover:bg-green-300 hover:cursor-pointer text-center">Register</button>
+			</form>
+		</div>
+	</div>
 	)
 }
 
-export default register
+export default Register
